fc2f9c4a1275ee876b23ac063dae2bf12725d46d
vdumas <pro.valentindumas@gmail.com>
2025-06-04 22:19:53 +0200

    Silly mistake without a method

--- src/main/java/com/example/expensereport/ExpenseReport.java
+++ src/main/java/com/example/expensereport/ExpenseReport.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+    DINNER, BREAKFAST, FIBER, CAR_RENTAL
 }
 
 class Expense {
@@ -14,15 +14,19 @@ class Expense {
 
 public class ExpenseReport {
     public void printReport(List<Expense> expenses) {
-        int total = 0;
-        int mealExpenses = 0;
-
         System.out.println("Expenses " + new Date());
 
+        int total = 0;
+        int mealExpenses = 0;
         for (Expense expense : expenses) {
             if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
                 mealExpenses += expense.amount;
             }
+
+            String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000
+                    || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
+
+            String leasureOverExpensesMarker = expense.type == ExpenseType.FIBER && expense.amount > 3000 ? "X" : " ";
 
             String expenseName = "";
             switch (expense.type) {
@@ -35,11 +39,11 @@ public class ExpenseReport {
                 case CAR_RENTAL:
                     expenseName = "Car Rental";
                     break;
+                case FIBER:
+                    expenseName = "Fiber";
+                    break;
             }
-
-            String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
-
-            System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
+            System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker + "\t" + leasureOverExpensesMarker);
 
             total += expense.amount;
         }
